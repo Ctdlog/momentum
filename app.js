bb0af2839f3762fdd6d293f96bd77a44b275674a
CTD <qpflapffhs76@gmail.com>
2021-07-16 13:50:48 +0900

    Objects

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+const player = {
+  name: "nico",
+  points: 10,
+  fat: true,
+};
 
-// Get Item from Array
-console.log(daysOfWeek[4]);
+console.log(player);
+player.lastName = "potato";
+player.points = 15;
 
-// Add one more day to the array
-daysOfWeek.push("sun");
-
-console.log(daysOfWeek);
+console.log(player);
