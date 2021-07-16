33de5480f59291bd778884e5559a46c15dd6ec80
CTD <qpflapffhs76@gmail.com>
2021-07-16 12:45:02 +0900

    Momentum Recap 2021

---
+++ app.js
@@ -0,0 +1,13 @@
+const a = 5;
+const b = 2;
+
+let myName = "nico";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello" + myName);
+
+myName = "nicolas";
+
+console.log("your name is " + myName);
